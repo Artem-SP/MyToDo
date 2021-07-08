20abc683cdccb0f8176ffc76eb4f3dc0973caa01
Artem Sukhorukov <54895933+Artem-SP@users.noreply.github.com>
2021-07-08 13:30:29 +0300

    sessionStorege support added

--- src/App.js
+++ src/App.js
@@ -2,33 +2,35 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-
-  const [todos, setTodos] = useState(
-   [
-    { id: 1, date: "2021-01-07", time: "07:42", todo: "tech-testing"},
-    { id: 2, date: "2021-01-07", time: "07:42", todo: "lang-testing" },
+  const [todos, setTodos] = useState([
+    { id: 1, date: "2021-01-07", time: "07:42", todo: "tech-testing" },
+    { id: 2, date: "2021-01-07", time: "07:42", todo: "lang-testing" }
   ]);
-
-
 
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
-  const [todo, setTodo] = useState("")
- 
-  useEffect(() => {
-localStorage.setItem('todos', JSON.stringify(todos))
-  },[todos])
+  const [todo, setTodo] = useState("");
 
-  const addTodo = event => {
- 
-    setTodos([...todos, {id: todos[todos.length-1].id + 1, date: date, time: time, todo: todo}])
-    }
+  useEffect(() => {
+    let raw = sessionStorage.getItem("todos");
+    setTodos(JSON.parse(raw));
+  }, []);
+  useEffect(() => {
+    sessionStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (event) => {
+    setTodos([
+      ...todos,
+      { id: todos[todos.length - 1].id + 1, date: date, time: time, todo: todo }
+    ]);
+  };
   let output = todos.map((it, index) => {
-let cls = ''
-it.id % 2 === 0 ? cls = 'oddRow' : cls = 'evenRow'
+    let cls = "";
+    it.id % 2 === 0 ? (cls = "oddRow") : (cls = "evenRow");
 
     return (
-<table className="table">
+      <table className="table">
         <td className={cls}>
           <tr>{it.id}</tr>
         </td>
@@ -41,14 +43,14 @@ it.id % 2 === 0 ? cls = 'oddRow' : cls = 'evenRow'
         <td className={cls}>
           <tr>{it.todo}</tr>
         </td>
-        </table>
+      </table>
     );
   });
 
   return (
     <div className="App">
       <h1>ToDo</h1>
-     
+
       <div>{output}</div>
       <div>
         <div>
@@ -56,8 +58,10 @@ it.id % 2 === 0 ? cls = 'oddRow' : cls = 'evenRow'
           <input
             type="date"
             value="date"
-            onInput={(e) => {setDate(e.target.value)
-            console.log('render from date')}}
+            onInput={(e) => {
+              setDate(e.target.value);
+              console.log("render from date");
+            }}
           />
           <div>Your date: {date}</div>
         </div>
@@ -66,20 +70,22 @@ it.id % 2 === 0 ? cls = 'oddRow' : cls = 'evenRow'
           <input
             type="time"
             value="time"
-            onInput={(e) => {setTime(e.target.value)
-              console.log('render from time')}}
+            onInput={(e) => {
+              setTime(e.target.value);
+              console.log("render from time");
+            }}
           />
           <div>Your time: {time}</div>
         </div>
         <div>
           <div>Set ToDo</div>
           <input
-          type="text"
+            type="text"
             value={todo}
             onInput={(e) => {
               setTodo(e.target.value);
-              console.log('render from todo')}}
-                
+              console.log("render from todo");
+            }}
           />
           <div>Your TODO: {todo}</div>
         </div>
